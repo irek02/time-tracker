3b1bd98d67bc8da6db8ee9cc0fb1fe15ab9bcbbf
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2023-05-09 21:23:41 -0400

    trying to get the lapsed time calculated correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,48 @@
-import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BehaviorSubject, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'time-tracker';
 
+  timer: moment.Moment | null = null;
+
+  // lapsed$: BehaviorSubject<string> = new BehaviorSubject('');
+  lapsed = '';
+
   constructor(
-    private modalService: NgbModal,
   ) {
   }
 
-  open(modal: any): void {
-    this.modalService.open(modal);
+  ngOnInit(): void {
+    setInterval(() => {
+      if (this.timer) {
+        this.timer.diff(moment());
+        const asMs = moment.duration(moment().diff(this.timer)).asMilliseconds();
+        const foo = moment.utc(asMs).format("hh:mm:ss");
+        this.lapsed = moment.utc(moment().diff(this.timer)).format("hh:mm:ss");
+        console.log(moment(this.timer).fromNow());
+        // this.lapsed = asMs.toString();
+      }
+    }, 200);
   }
+
+  toggle(): void {
+
+    if (!this.timer) {
+      this.timer = moment();
+    } else {
+      this.timer = null;
+    }
+
+  }
+
 
 }
