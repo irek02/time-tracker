29e51a7cd454f14793d7b05b49e70b0e911c3940
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2023-05-12 22:02:32 -0400

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import * as moment from 'moment';
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BehaviorSubject, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +9,9 @@ export class AppComponent implements OnInit {
 
   title = 'time-tracker';
 
-  timer: moment.Moment | null = null;
-
-  // lapsed$: BehaviorSubject<string> = new BehaviorSubject('');
-  lapsed = '';
+  startedTimeMs: number | null = null;
+  elapsed = '';
+  entries: { start: number; stop: number }[] = [];
 
   constructor(
   ) {
@@ -23,41 +19,43 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     setInterval(() => {
-      if (this.timer) {
-        const duration = moment.duration(this.timer.diff(this.getNow()));
-        const asSec = Math.abs(Math.round(duration.asSeconds()));
-
-        const hour = Math.floor(asSec / 60 / 60);
-        const min = Math.floor(asSec / 60 - hour * 60);
-        const sec = Math.floor(asSec - min * 60 - hour * 60 * 60);
-
-        const hourStr = hour < 10 ? '0' + hour : hour;
-        const minStr = min < 10 ? '0' + min : min;
-        const secStr = sec < 10 ? '0' + sec : sec;
-
-
-        this.lapsed = `${hourStr}:${minStr}:${secStr}`;
+      if (this.startedTimeMs) {
+        this.elapsed = this.getDuration(this.startedTimeMs, this.getNowMs());
       }
-    }, 1000);
+    }, 300);
   }
 
-  getNow() {
+  getNowMs(): number {
 
-    // return moment(1683613935000);
-
-    return moment();
+    return Date.now();
 
   }
 
   toggle(): void {
 
-    if (!this.timer) {
-      this.timer = moment();
+    if (!this.startedTimeMs) {
+      this.startedTimeMs = Date.now();
     } else {
-      this.timer = null;
+      this.entries.push({ start: this.startedTimeMs, stop: this.getNowMs() });
+      this.startedTimeMs = null;
     }
 
   }
 
+  getDuration(startMs: number, stopMs: number): string {
+
+    const asSec = Math.abs((stopMs - startMs) / 1000);
+
+    let hour = Math.floor(asSec / 60 / 60);
+    let min = Math.floor(asSec / 60 - hour * 60);
+    let sec = Math.floor(asSec - min * 60 - hour * 60 * 60);
+
+    const hourStr = hour > 9 ? hour : '0' + hour;
+    const minStr = min > 9 ? min : '0' + min;
+    const secStr = sec > 9 ? sec : '0' + sec;
+
+    return `${hourStr}:${minStr}:${secStr}`;
+
+  }
 
 }
