d44864ea1489c9829f02e8a542828e5e3442e94b
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2023-06-09 20:58:50 -0400

    trying to work out the charts

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,26 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, computed } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss']
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit {
 
-  public barChartLegend = true;
-  public barChartPlugins = [];
+  barChartLegend = true;
+  barChartPlugins = [];
 
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
-    datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
-    ]
-  };
+  barChartData: Signal<ChartConfiguration<'bar'>['data']>;
+  // public barChartData: ChartConfiguration<'bar'>['data'] = {
+  //   labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+  //   datasets: [
+  //     { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+  //     { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+  //   ]
+  // };
 
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+  barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
+    scales: {
+      x: {
+          stacked: true
+      },
+      y: {
+          stacked: true
+      }
+    },
   };
+
+  constructor(
+    public dataService: DataService,
+  ) {
+    this.barChartData = computed(() => {
+
+      const res: any = {};
+
+      this.dataService.entries().forEach(entry => {
+        const proj = entry.project?.name || 'no-project';
+        if (!res[proj]) {
+          res[proj] = [];
+        }
+        res[proj].push(Math.ceil((entry.stop - entry.start) / 1000 / 60));
+      });
+
+      console.log(res);
+
+      return {
+        labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+        datasets: [
+          { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+          { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+        ]
+      };
+
+    });
+  }
+
+  ngOnInit() {
+
+
+  }
 
 }
