6ebbd1a991a749aa68a718f84dc6228569f7bad6
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2023-06-14 19:55:52 -0400

    date picker works

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Signal, computed, signal } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
-import { DataService } from 'src/app/services/data.service';
+import { DataService, Entry } from 'src/app/services/data.service';
 import * as moment from 'moment';
 import * as dayjs from 'dayjs'
-
-interface Entry {
-  date: string;
-  duration: number;
-  project: string;
-}
 
 @Component({
   selector: 'app-reports',
@@ -20,60 +14,72 @@ export class ReportsComponent implements OnInit {
   ranges: any = {
     'Today': [dayjs(), dayjs()],
     'Yesterday': [dayjs().subtract(1, 'days'), dayjs().subtract(1, 'days')],
-    'Last 7 Days': [dayjs().subtract(6, 'days'), dayjs()],
-    'Last 30 Days': [dayjs().subtract(29, 'days'), dayjs()],
+    'This Week': [dayjs().startOf('week'), dayjs()],
+    'Last Week': [dayjs().subtract(1, 'week').startOf('week'), dayjs().subtract(1, 'week').endOf('week')],
     'This Month': [dayjs().startOf('month'), dayjs().endOf('month')],
     'Last Month': [dayjs().subtract(1, 'month').startOf('month'), dayjs().subtract(1, 'month').endOf('month')]
   }
-
+  barChartData!: Signal<ChartConfiguration<'bar'>['data']>;
+  selectedDates = signal({ startDate: dayjs().startOf('week'), endDate: dayjs() });
   barChartLegend = true;
-  barChartData!: ChartConfiguration<'bar'>['data'];
   barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
     scales: {
-      x: {
-        stacked: true
+      x: { stacked: true },
+      y: { stacked: true },
+    },
+    plugins: {
+      legend: {
+        position: 'bottom',
       },
-      y: {
-        stacked: true
-      }
     },
   };
 
   constructor(private dataService: DataService) {}
 
-  foo(e: {startDate: dayjs.Dayjs, endDate: dayjs.Dayjs }) {
-    console.log(e);
-  }
-
   ngOnInit() {
 
-    this.barChartData = this.generateChartData();
+    this.barChartData = computed(() => {
+      const { startDate, endDate } = this.selectedDates();
+      const filteredEntries = this.dataService.entries()
+        .filter(entry => entry.start > startDate.valueOf() && entry.start < endDate.valueOf())
+        // Sort by putting more recent entries to the end.
+        .sort((a, b) => a.start > b.start ? 1 : -1);
+      return this.generateChartData(filteredEntries);
+    });
 
   }
 
-  private generateChartData(): ChartConfiguration<'bar'>['data'] {
-    const entries: Entry[] = this.dataService.entries().map(entry => ({
+  onCalendarChange(e: { startDate: dayjs.Dayjs, endDate: dayjs.Dayjs }) {
+
+    if (e.endDate && e.startDate) {
+      this.selectedDates.set(e);
+    }
+
+  }
+
+  private generateChartData(entries: Entry[]): ChartConfiguration<'bar'>['data'] {
+    const newEntries: { date: string, duration: number, project: string }[] = entries.map(entry => ({
       date: moment(entry.start).format('MMM D'),
       duration: Math.ceil((entry.stop - entry.start) / 1000 / 60 / 60),
       project: entry.project?.name || 'no-project',
     }));
 
-    const entriesByProject: { [key: string]: Entry[] } = {};
-    entries.forEach(entry => {
+    const entriesByProject: { [key: string]: { date: string, duration: number, project: string }[] } = {};
+    newEntries.forEach(entry => {
       if (!entriesByProject[entry.project]) {
         entriesByProject[entry.project] = [];
       }
       entriesByProject[entry.project].push(entry);
     });
 
-    const dates = Array.from(new Set(entries.map(entry => entry.date)));
+    const dates = Array.from(new Set(newEntries.map(entry => entry.date)));
     const datasets = [];
     for (let project in entriesByProject) {
       const dataset: any = {
         data: [],
         label: project,
-        backgroundColor: this.dataService.projects().find(p => p.name === project)?.color,
+        backgroundColor: this.dataService.projects().find(p => p.name === project)?.color || 'purple',
       };
 
       for (let date of dates) {
